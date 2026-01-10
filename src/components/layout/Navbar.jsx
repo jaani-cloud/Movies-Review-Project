c0df5c1802e401da8c4d826d7fa4223dc31a4e89
heredikshit-ux <heredikshit@gmail.com>
2026-01-11 04:15:25 +0530

    add logout to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,17 @@
 import { Search } from "lucide-react"
 
+// import { useState } from "react"
+import {getCurrentUser,logout} from "../../services/authService"
+import { useNavigate } from "react-router-dom"
+
 export default function Navbar({searchQuery, setSearchQuery}) {
+    const currentUser = getCurrentUser();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+    }
 
     return (
         <nav className="fixed top-0 left-0 right-0 z-50 p-4 text-white bg-slate-900 flex items-center gap-4">
@@ -14,6 +25,10 @@ export default function Navbar({searchQuery, setSearchQuery}) {
                     onChange={(e) => setSearchQuery(e.target.value)}
                     className="Search w-full pl-10"
                 />
+            </div>
+            <div className="flex gap-3 items-center">
+                <p className="font-semibold">👤 {currentUser?.firstName} {currentUser?.lastName}</p>
+                <button onClick={handleLogout} className="bg-red-600 px-4 py-2 rounded-lg hover:bg-red-700">Logout</button>
             </div>
         </nav>
     )
