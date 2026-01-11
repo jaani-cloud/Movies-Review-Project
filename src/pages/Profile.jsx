8edc4b0f39156ece29218e2bdb78c22beaab48c0
heredikshit-ux <heredikshit@gmail.com>
2026-01-11 18:59:55 +0530

    some improvments in user profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { getCurrentUser } from "../services/authService"
 import { Link } from "react-router-dom"
+import { Instagram, Youtube } from "lucide-react"
 
 export default function Profile() {
     const currentUser = getCurrentUser()
@@ -83,16 +84,7 @@ export default function Profile() {
 
                 <div className="mb-4">
                     <label htmlFor="" className="Profile-label">Email</label>
-                    {isEdited ? (
-                        <input
-                            type="email"
-                            value={formData.email}
-                            onChange={(e) => setFormData({ ...setFormData, email: e.target.value })}
-                            className="Profile-input"
-                        />
-                    ) : (
-                        <p className="text-lg">{currentUser.email}</p>
-                    )}
+                    <p className="text-lg">{currentUser.email}</p>
                 </div>
 
                 <div className="mb-4">
@@ -134,12 +126,25 @@ export default function Profile() {
                     {isEdited ? (
                         <input
                             type="text"
+                            placeholder="Enter the username"
                             value={formData.instagram}
                             onChange={(e) => setFormData({ ...setFormData, instagram: e.target.value })}
                             className="Profile-input"
                         />
                     ) : (
-                        <p className="text-lg">{currentUser.instagram || "Not set"}</p>
+                        currentUser.instagram ? (
+                            <>
+                                <a className="flex items-center gap-2 text-pink-400 hover:text-pink-300 text-lg"
+                                    href={`https://instagram.com/${currentUser.instagram.replace("@", "")}`}
+                                    target="_blank"
+                                >
+                                    <Instagram size={20} />
+                                    @{currentUser.instagram.replace("@", "")}
+                                </a>
+                            </>) : (
+                            <>
+                                <p className="text">Not Set</p>
+                            </>)
                     )}
                 </div>
 
@@ -153,7 +158,21 @@ export default function Profile() {
                             className="Profile-input"
                         />
                     ) : (
-                        <p className="text-lg">{currentUser.youtube || "Not set"}</p>
+                        currentUser.youtube ? (
+                            <>
+                                <a className="flex items-center gap-2 text-red-400 hover:text-red-300 text-lg"
+                                    href={currentUser.youtube.startsWith("http") ? currentUser.youtube : `https://youtube.com/@${currentUser.youtube.replace("@", "")}`}
+                                    target="_blank"
+                                >
+                                    <Youtube size={20}/>
+                                    {currentUser.youtube.startsWith("http") ? "Visit Channel" : `@${currentUser.youtube.replace("@", "")}`}
+                                </a>
+                            </>
+                        ) : (
+                            <>
+
+                            </>
+                        )
                     )}
                 </div>
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -73,10 +73,6 @@ export default function Settings() {
 
                     <button className="bg-blue-600 px-6 py-2 rounded-lg hover:bg-blue-700" onClick={handlePasswordChange}>Update Password</button>
                 </div>
-
-                <div className="bg-slate-900 p-6 rounded-lg">
-                    <h2 className="text-2xl font-bold mb-4">Change Email</h2>
-                </div>
             </div>
         </div>
     )
