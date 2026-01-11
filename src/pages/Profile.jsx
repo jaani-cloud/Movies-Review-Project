dd1c023cd1be8960d0634aae8469ba88ec12ce5b
heredikshit-ux <heredikshit@gmail.com>
2026-01-11 06:06:41 +0530

    update user profile info page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -17,6 +17,16 @@ export default function Profile() {
             <h1 className="text-4xl font-bold mb-8">MyProfile</h1>
 
             <div className="bg-slate-900 p-6 rounded-lg max-w-2xl">
+
+                <div className="mb-4">
+                    <label htmlFor="" className="text-slate-400 text-sm block mb-1">Profile Photo</label>
+                    <div className="w-24 bg-slate-800 h-24 rounded-full flex items-center justify-center text-4xl">
+                        {currentUser.profilePhoto ? (
+                            <img src={currentUser.profilePhoto} alt="" className="w-24 h-24 rounded-full object-cover"/>
+                        ) : "👤"}
+                    </div>
+                </div>
+
                 <div className="mb-4">
                     <label htmlFor="" className="text-slate-400 text-sm block mb-1">Username</label>
                     <p className="text-lg">@{currentUser.username}</p>
@@ -27,9 +37,24 @@ export default function Profile() {
                     <p className="text-lg">{currentUser.email}</p>
                 </div>
 
-                <div>
+                <div className="mb-4">
                     <label htmlFor="" className="text-slate-400 text-sm block mb-1">Name</label>
                     <p className="text-lg">{currentUser.firstName} {currentUser.lastName}</p>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="" className="text-slate-400 text-sm block mb-1">Date of Birth</label>
+                    <p className="text-lg">{currentUser.dob || "Not set"}</p>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="" className="text-slate-400 text-sm block mb-1">Instagram</label>
+                    <p className="text-lg">{currentUser.instagram || "Not set"}</p>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="" className="text-slate-400 text-sm block mb-1">Youtube</label>
+                    <p className="text-lg">{currentUser.youtube || "Not set"}</p>
                 </div>
             </div>
         </div>
